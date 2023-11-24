86eaf98c594848620b3fad4b4e92bb3f6f9c59ce
Timurrko <sharapov.team@yandex.ru>
2023-11-25 02:22:57 +0500

    Бот полностью опрашивает пользователя, записывает ответы в словарь и при следующем обращении даёт пользователю понять, что помнит его

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ bot = telebot.TeleBot('6985946769:AAE-bKL3CdkCSAGm068r3v1dCxtowtXrahk')
 
 users = {}
 
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     user_id = message.from_user.id
@@ -13,15 +14,68 @@ def send_welcome(message):
         msg = bot.reply_to(message, "Привет! Как тебя зовут?")
         bot.register_next_step_handler(msg, process_name_step)
 
+
 def process_name_step(message):
     name = message.text
     user_id = message.from_user.id
+    users[user_id] = {'name': name}
     msg = bot.reply_to(message, 'Какая у тебя фамилия?')
-    bot.register_next_step_handler(msg, process_surname_step, name, user_id)
+    bot.register_next_step_handler(msg, process_surname_step)
 
-def process_surname_step(message, name, user_id):
+
+def process_surname_step(message):
     surname = message.text
-    users[user_id] = {'name': name, 'surname': surname}
-    bot.reply_to(message, f'Спасибо, {name} {surname}!')
+    user_id = message.from_user.id
+    users[user_id]['surname'] = surname
+    msg = bot.reply_to(message, 'Какой у тебя email?')
+    bot.register_next_step_handler(msg, process_email_step)
+
+
+def process_email_step(message):
+    email = message.text
+    user_id = message.from_user.id
+    users[user_id]['email'] = email
+    msg = bot.reply_to(message, 'Какой у тебя номер телефона?')
+    bot.register_next_step_handler(msg, process_phone_step)
+
+
+def process_phone_step(message):
+    phone = message.text
+    user_id = message.from_user.id
+    users[user_id]['phone'] = phone
+    msg = bot.reply_to(message, 'Какие у тебя увлечения?')
+    bot.register_next_step_handler(msg, process_hobbies_step)
+
+
+def process_hobbies_step(message):
+    hobbies = message.text
+    user_id = message.from_user.id
+    users[user_id]['hobbies'] = hobbies
+    msg = bot.reply_to(message, 'Сколько тебе лет?')
+    bot.register_next_step_handler(msg, process_age_step)
+
+
+def process_age_step(message):
+    age = message.text
+    user_id = message.from_user.id
+    users[user_id]['age'] = age
+    msg = bot.reply_to(message, 'В какой ты учебной группе?')
+    bot.register_next_step_handler(msg, process_group_step)
+
+
+def process_group_step(message):
+    group = message.text
+    user_id = message.from_user.id
+    users[user_id]['group'] = group
+    msg = bot.reply_to(message, 'Какие у тебя навыки?')
+    bot.register_next_step_handler(msg, process_skills_step)
+
+
+def process_skills_step(message):
+    skills = message.text
+    user_id = message.from_user.id
+    users[user_id]['skills'] = skills
+    bot.reply_to(message, f'Спасибо, {users[user_id]["name"]} {users[user_id]["surname"]}!')
+
 
 bot.polling()
